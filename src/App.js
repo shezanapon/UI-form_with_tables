9274a4c400295696aa45cfd87ec9a35df7ec7948
Shezan Mahbub <shezanmahbub94@gmail.com>
2023-03-21 13:36:11 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import UiForm from "./components/UiForm";
+
+function App() {
+  return (
+    <div className="App">
+      <UiForm></UiForm>
+    </div>
+  );
+}
+
+export default App;
